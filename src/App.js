fb4a79c3f3b2a527f7c4feac883bd83ee5a33368
salihsungurcelik <salihcelik099@gmail.com>
2024-08-16 22:02:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  //Verilerin tutulacağı stateyi ayarlama
+  const [productList, setProductList] = useState([]);
+  // //Eski Metod
+  //   useEffect(() => {
+  //     fetch("https://dummyjson.com/products")
+  //       .then((res) => res.json())
+  //       .then((productList) =>
+  //         console.log("ikinci dönen yanıt", productList.products)
+  //       )
+  //       .catch((err) => console.log("Veri çekme hatası", err));
+  //   }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => setProductList(res?.data?.products))
+      .catch((err) => console.log("axiosget hatası", err));
+  }, []);
+  // console.log("Ürünler State", productList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ürünler</h1>
+      <ul>
+        {productList?.map((product) => (
+          <li key={product?.id}>{product?.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
